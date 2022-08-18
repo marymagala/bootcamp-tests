5207326fda2ce5070292d668bc688d2d0ef67da2
unknown <marymagalam@gmail.com>
2022-08-18 20:25:29 +0200

    tests

--- test/greet.tests.js
+++ test/greet.tests.js
@@ -2,7 +2,6 @@ describe('My greet function' , function(){
     it('should show a hello message and the name of a person' , function(){
         assert.equal(greet('Bob'), 'Hello, Bob');
 
-        assert.deepEqual([2,2],[2,2]);
     });
 
 });
