3ec5cee86e850564cf7a1ed774aa1ecfa78982f2
unknown <marymagalam@gmail.com>
2022-08-25 00:50:36 +0200

    tests

--- test/greet.tests.js
+++ test/greet.tests.js
@@ -1,5 +1,5 @@
 describe('My greet function' , function(){
-    it('should show a hello message and the name of a person' , function(){
+    it('It should show a hello message and the name of a person being' , function(){
         assert.equal(greet('Bob'), 'Hello, Bob');
 
     });
